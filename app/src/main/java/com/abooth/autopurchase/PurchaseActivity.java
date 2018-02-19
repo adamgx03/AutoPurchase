1c023f9126104020566073f7306a929ae1c438ed
Adam Booth <abooth132@gmail.com>
2018-02-19 15:49:46 -0600

    Initial commit

---
+++ app/src/main/java/com/abooth/autopurchase/PurchaseActivity.java
@@ -0,0 +1,13 @@
+package com.abooth.autopurchase;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PurchaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.purchase_layout);
+    }
+}
